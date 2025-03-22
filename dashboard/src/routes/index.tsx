70550a8214a270a4d98c2d0af0a2f4ebbc7779b9
Nirwan Rai <nirwanrai6969@gmail.com>
2025-03-22 10:19:36 +0545

    added tanstack router

---
+++ dashboard/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3 className="text-amber-400">Welcome Home!</h3>
+    </div>
+  );
+}
--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    tailwindcss(),
+    react(),
+  ],
 });
