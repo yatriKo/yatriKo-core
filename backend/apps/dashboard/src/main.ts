bb776e2322ff3537a61484b4fd4c58d05e1ca512
Nirwan Rai <nirwanrai6969@gmail.com>
2025-04-07 09:08:03 +0545

    fixed cors error dashboard side

--- backend/apps/dashboard/src/main.ts
+++ backend/apps/dashboard/src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(DashboardModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.port ?? 3001);
 }
 bootstrap();
